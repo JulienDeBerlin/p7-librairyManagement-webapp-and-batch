93bab8e2add5d1db9fb11358681ce5e72ed76678
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-04-13 09:46:56 +0200

    set-up thymeleaf

---
+++ p7-webapp-presentation/src/main/java/com/berthoud/p7/webapp/WebserviceApp.java
@@ -0,0 +1,18 @@
+package com.berthoud.p7.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceApp.class, args);
+
+
+    }
+
+
+
+}
