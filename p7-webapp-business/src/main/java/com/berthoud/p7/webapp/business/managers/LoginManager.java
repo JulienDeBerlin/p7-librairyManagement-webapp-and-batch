11b32258d616965a5acc1983039bb7383e456950
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-05-22 20:03:53 +0200

    App method in LoginManager to sort out closed Loans from a list of Loans

--- p7-webapp-business/src/main/java/com/berthoud/p7/webapp/business/managers/LoginManager.java
+++ p7-webapp-business/src/main/java/com/berthoud/p7/webapp/business/managers/LoginManager.java
@@ -7,9 +7,10 @@ import org.springframework.stereotype.Service;
 import p7.webapp.model.beans.Customer;
 import p7.webapp.model.beans.Loan;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 
 @Service
@@ -25,29 +26,54 @@ public class LoginManager {
      * @param password the password of the {@link Customer}
      * @return If the password matches with the email, the corresponding {@link Customer} object is returned.
      */
-    public Customer loginCustomer (String email, String password){
-        Utils.loggerWebappBusiness.trace("entering method loginCustomer with param email = " + email );
+    public Customer loginCustomer(String email, String password) {
+        Utils.loggerWebappBusiness.trace("entering method loginCustomer with param email = " + email);
 
         Customer customer = customerDAO.getCustomer(email, password);
-        List<Loan> loans= customer.getLoans();
+        List<Loan> loans = customer.getLoans();
+        removeClosedLoans(loans);
         Collections.sort(loans);
         customer.setLoans(loans);
 
         return customer;
     }
 
-
-    public Customer refreshCustomer(String email){
+    /**
+     * This method is used to reload a {@link Customer}, after a change has been made (for instance: extension of a loan)
+     *
+     * @param email the email of the {@link Customer}
+     * @return the updated {@link Customer} object
+     */
+    public Customer refreshCustomer(String email) {
         Utils.loggerWebappBusiness.trace("entering method refreshCustomer with param email = " + email);
 
         Customer customer = customerDAO.refreshCustomer(email);
-        List<Loan> loans= customer.getLoans();
+        List<Loan> loans = customer.getLoans();
         Collections.sort(loans);
         customer.setLoans(loans);
         return customer;
     }
 
 
+    /**
+     * This method takes as input a list of {@link Loan} objects and remove from the list
+     * all loans which are closed (= book has been returned)
+     *
+     * @param loanList the list of {@link Loan} to be sorted
+     *
+     * @return a list with only open Loans.
+     */
+    public List<Loan> removeClosedLoans(List<Loan> loanList) {
+        Iterator<Loan> i = loanList.iterator();
+        LocalDate loanOpen = LocalDate.of(1900, 01, 01);
+        while (i.hasNext()) {
+            Loan loan = i.next();
+            if (!(loan.getDateBack().isEqual(loanOpen))){
+                i.remove();
+            }
+        }
+        return loanList;
+    }
 
 
 }
